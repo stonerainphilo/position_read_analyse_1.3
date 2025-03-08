f1d1cd241bd35a1f7a9153dd80c3404f6899faa9
Stone rainphilosophy <2819528523@qq.com>
2025-03-08 14:51:44 +0800

    Rearranged python file location for ALL_IN_ONE: 	new file:   ALL_IN_ONE/Pythia8/__pycache__/analyse_data.cpython-310.pyc 	new file:   ALL_IN_ONE/Pythia8/__pycache__/detection.cpython-310.pyc 	modified:   ALL_IN_ONE/Pythia8/__pycache__/loop.cpython-310.pyc 	modified:   ALL_IN_ONE/Pythia8/__pycache__/one_key_run.cpython-310.pyc 	modified:   ALL_IN_ONE/Pythia8/__pycache__/run_save.cpython-310.pyc 	modified:   ALL_IN_ONE/Pythia8/analyse_data.py 	modified:   ALL_IN_ONE/Pythia8/loop.py 	modified:   ALL_IN_ONE/Pythia8/one_key_run.py 	modified:   ALL_IN_ONE/Pythia8/run_save.py 	modified:   ALL_IN_ONE/all_in_one.ipynb 	deleted:    combine.py 	deleted:    detection.py 	modified:   generate_save_analyse_judge_for_pythia83XX.ipynb 	deleted:    loop.py 	modified:   lsd.ipynb 	deleted:    one_key_run.py 	deleted:    plotting.py 	deleted:    run_save.py

--- ALL_IN_ONE/Pythia8/analyse_data.py
+++ ALL_IN_ONE/Pythia8/analyse_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 def see_ctau_2(csv_file):
     df = pd.read_csv(csv_file)
     i = 0
@@ -12,4 +12,12 @@ def see_ctau_2(csv_file):
         else:
             j = j + 1    
     return i, j
+
+def print_max_min_log10_sin_theta_2(filename):
+    df = pd.read_csv(filename)
+    theta = df['theta_input']
+    sin_square_theta = np.sin(theta)**2
+    max_ = np.log(max(sin_square_theta))/np.log(10)
+    min_ = np.log(min(sin_square_theta))/np.log(10)
+    return max_, min_
     
--- ALL_IN_ONE/Pythia8/loop.py
+++ ALL_IN_ONE/Pythia8/loop.py
@@ -1,7 +1,7 @@
 from functions_for_run import generate_randomseed
 import numpy as np
 from tqdm import tqdm
-from run_save import run_save_main41_csv, run_save_main41_csv_all_br
+from run_save import run_save_main41_csv, run_save_main41_csv_all_br, run_save_main131_csv_all_br_main131
 import pandas as pd
 import os
 
@@ -51,19 +51,19 @@ def loop_mass_ctau(mass_lower_lim, mass_upper_lim, mass_array_length,
     out_dir_name = os.path.dirname(out_path_name_LLP_data)
     return out_dir_name
 
-def loop_mass_ctau_certain_seed(mass_lower_lim, mass_upper_lim, mass_array_length, 
-                 ctau_lower_lim, ctau_upper_lim, ctau_array_length, br, seed_array, out_put_path, main41_path):
-    total_iterations = len(seed_array) * ctau_array_length * mass_array_length
+# def loop_mass_ctau_certain_seed(mass_lower_lim, mass_upper_lim, mass_array_length, 
+#                  ctau_lower_lim, ctau_upper_lim, ctau_array_length, br, seed_array, out_put_path, main41_path):
+#     total_iterations = len(seed_array) * ctau_array_length * mass_array_length
 
-    with tqdm(total=total_iterations) as pbar:
-        for seed in seed_array:
-            for taus in np.logspace(ctau_lower_lim, ctau_upper_lim, ctau_array_length):
-                for mass in np.linspace(mass_lower_lim, mass_upper_lim, mass_array_length):
-                    out_path_name_LLP_data = run_save_main41_csv(mass, seed, br, taus, out_put_path, main41_path)[0]
+#     with tqdm(total=total_iterations) as pbar:
+#         for seed in seed_array:
+#             for taus in np.logspace(ctau_lower_lim, ctau_upper_lim, ctau_array_length):
+#                 for mass in np.linspace(mass_lower_lim, mass_upper_lim, mass_array_length):
+#                     out_path_name_LLP_data = run_save_main41_csv(mass, seed, br, taus, out_put_path, main41_path)[0]
                     
-                    pbar.update(1)
-    out_dir_name = os.path.dirname(out_path_name_LLP_data)
-    return out_dir_name
+#                     pbar.update(1)
+#     out_dir_name = os.path.dirname(out_path_name_LLP_data)
+#     return out_dir_name
 
 def loop_mass_ctau_given_by_csv(csv_file, br, seed_amount, out_put_path, main41_path):
     df = pd.read_csv(csv_file)
@@ -87,6 +87,21 @@ def loop_mass_ctau_br_given_by_csv(csv_file, br, seed_amount, out_put_path, main
                                                                                                                              df['Br_Hee'], df['Br_HKK'], df['Br_HPiPi'], df['Br_Htautau'], df['Br_HGluon'],
                                                                                                                              df['Br_Hmumu'], df['Br_Hgaga'], df['Br_H4Pi'], df['Br_Hss'], df['Br_Hcc'], df['theta'], df['Decay_width_total']):
                 out_put_name_LLP_data = run_save_main41_csv_all_br(mH, seed, br, taus, out_put_path, main41_path, Br_Hee, Br_HKK, Br_HPIPI, Br_Htautau, Br_HGluon,Br_Hmumu, Br_Hgaga, Br_H4Pi, Br_Hss, Br_Hcc, theta, Decay_Width_Total)[0]
+                
+                pbar.update(1)
+    out_dir_name = os.path.dirname(out_put_name_LLP_data)
+    
+    return out_dir_name
+
+def loop_mass_ctau_br_given_by_csv_main131(csv_file, br, seed_amount, out_put_path, main131_path):
+    df = pd.read_csv(csv_file)
+    total_iterations = seed_amount * len(df['mH'])
+    with tqdm(total=total_iterations) as pbar:
+        for seed in generate_randomseed(seed_amount):
+            for mH, taus, Br_Hee, Br_HKK, Br_HPIPI, Br_Htautau, Br_HGluon,Br_Hmumu, Br_Hgaga, Br_H4Pi, Br_Hss, Br_Hcc, theta, Decay_Width_Total in zip(df['mH'], df['ltime'],
+                                                                                                                             df['Br_Hee'], df['Br_HKK'], df['Br_HPiPi'], df['Br_Htautau'], df['Br_HGluon'],
+                                                                                                                             df['Br_Hmumu'], df['Br_Hgaga'], df['Br_H4Pi'], df['Br_Hss'], df['Br_Hcc'], df['theta'], df['Decay_width_total']):
+                out_put_name_LLP_data = run_save_main131_csv_all_br_main131(mH, seed, br, taus, out_put_path, main131_path, Br_Hee, Br_HKK, Br_HPIPI, Br_Htautau, Br_HGluon,Br_Hmumu, Br_Hgaga, Br_H4Pi, Br_Hss, Br_Hcc, theta, Decay_Width_Total)[0]
                 
                 pbar.update(1)
     out_dir_name = os.path.dirname(out_put_name_LLP_data)
